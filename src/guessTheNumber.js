53be45a5d8bf18dead32275e67d168ee168782d8
Jose Vazquez <zezemanolo@Joses-MacBook-Pro.local>
2017-02-07 11:00:33 -0800

    adds guessTheNumber v2.0 to and test

--- src/guessTheNumber.js
+++ src/guessTheNumber.js
@@ -1,18 +1,16 @@
-const min = Math.ceil(1)
-const max = Math.floor(100)
-const randomNum = Math.floor(Math.random() * (max - min + 1) + min)
-const getUserInput = document.querySelector('input#guess-number')
-const guessTheNumber = (inputValue) => {
-  inputValue = getUserInput.value
-  if(inputValue.toLowerCase() === 'exit') {
-    alert('Game Over! Reload browser to restart game!')
-  } else if(inputValue === '' || isNaN(inputValue)) {
-    alert('Only integers RAWR!!!')
-  } else if(Number(inputValue) < Number(randomNum)) {
-    alert(('You\'re kinda low! OMG!!!'))
-  } else if(Number(inputValue) > Number(randomNum)) {
-    alert(('You\'re flying kind of high buddy LAWL!!!'))
-  } else {
-    alert('YOU WIN!!! Reload browser to restart game!')
+const randomNum = Math.floor(Math.random() * 100)
+export const guessTheNumber = (input) => {
+  if(Number.isInteger(input) && input < randomNum) {
+    return 'Your guess is kinda low'
+  } else if(Number.isInteger(input) && input > randomNum) {
+    return 'You too high!!!'
+  } else if(input === randomNum) {
+    return 'Yeee you win!!!'
+    process.exit(0)
+  } else if(input.toLowerCase() === 'exit'){
+    process.stdout.write('game over! way to quit')
+    process.exit(0)
+  } else if(!input || isNaN(input)) {
+    return 'Insert only integers RAWR!!!'
   }
 }
