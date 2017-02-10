8c300055105a11e8f817a1da324abf387a219038
Jose Vazquez <zezemanolo@Joses-MacBook-Pro.local>
2017-02-10 09:55:33 -0800

    removed variables for test since they are not needed there

--- tests/guessTheNumber_test.js
+++ tests/guessTheNumber_test.js
@@ -6,19 +6,15 @@ describe('guessTheNumber()', () => {
     expect(guessTheNumber).to.be.a('function')
   })
   it('alerts user if input is not an integer', () => {
-    const randomNum = Math.floor(Math.random() * 100)
     expect(guessTheNumber('d')).to.equal('Insert only integers RAWR!!!')
   })
   it('alerts user if parameter is empty string', () => {
-    const randomNum = Math.floor(Math.random() * 100)
     expect(guessTheNumber('')).to.equal('Insert only integers RAWR!!!')
   })
   it('returns string if input < randomNum', () => {
-    const randomNum = Math.floor(Math.random() * 100)
     expect(guessTheNumber(1)).to.equal('Your guess is kinda low')
   })
   it('returns string if input > randomNum', () => {
-    const randomNum = Math.floor(Math.random() * 100)
     expect(guessTheNumber(100)).to.equal('You too high!!!')
   })
 })
